d3df0e0db239255c98a5c109a919c61b29699444
Gabriel Leandro <gabrieleandro.dev@gmail.com>
2022-04-03 17:37:16 -0300

    Add Django restframework Simple JWT

--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     # third-party apps
     'rest_framework',
+    'djangorestframework-simplejwt',
+    #
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
